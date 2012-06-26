b7fbabdca4e14acbb5df919128ab40b1797e81fd
savevjr <savevjr@gmail.com>
2012-06-26 11:54:25 +0300

    weird identificator

--- Mirror/MainWindow.xaml.vb
+++ Mirror/MainWindow.xaml.vb
@@ -3,8 +3,8 @@
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         Dim vb = New Core.StringBuild.VBStringBuilder
         AssBrowser.Items.Add(New Core.Element(Me.GetType.Assembly, vb))
-        For Each ass In Me.GetType.Assembly.GetReferencedAssemblies()
-            AssBrowser.Items.Add(New Core.Element(Reflection.Assembly.Load(ass.FullName), vb))
+        For Each asse In Me.GetType.Assembly.GetReferencedAssemblies()
+            AssBrowser.Items.Add(New Core.Element(Reflection.Assembly.Load(asse.FullName), vb))
         Next
     End Sub
 End Class
