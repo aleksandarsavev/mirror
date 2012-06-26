9b3b26acf65621aea21795035fc91a1aea7bcfda
Aleksandar <savevjr@gmail.com>
2012-06-26 11:41:07 +0300

    window project

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class MainWindow 
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim vb = New Core.StringBuild.VBStringBuilder
+        AssBrowser.Items.Add(New Core.Element(Me.GetType.Assembly, vb))
+        For Each ass In Me.GetType.Assembly.GetReferencedAssemblies()
+            AssBrowser.Items.Add(New Core.Element(Reflection.Assembly.Load(ass.FullName), vb))
+        Next
+    End Sub
+End Class
